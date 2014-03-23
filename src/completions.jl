b97d35b639ad7f34f22c8773107ce0e89fdf50d3
Mike Innes <mike.j.innes@gmail.com>
2014-03-23 01:59:56 +0000

    packages function

--- src/completions.jl
+++ src/completions.jl
@@ -5,6 +5,9 @@ module_usings(mod) = ccall(:jl_module_usings, Any, (Any,), mod)
 accessible_names(mod = Main) =
   @as _ mod [_, module_usings(_)...] map(names, _) [_...]
 
+packages() =
+  @>> Pkg.dir() readdir filter(x->!ismatch(r"^\.|^METADATA", x))
+
 handle("editor.julia.hints") do req, data
   editor_command(req, "hints", {:hints => [string(n) for n in accessible_names()]})
 end
