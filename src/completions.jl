acb9103862bfa2d0bbc4190a89083520a8d62d96
Mike Innes <mike.j.innes@gmail.com>
2014-03-23 01:36:21 +0000

    only use actually accessible names

--- src/completions.jl
+++ src/completions.jl
@@ -2,17 +2,9 @@
 
 module_usings(mod) = ccall(:jl_module_usings, Any, (Any,), mod)
 
-# Reasons to use Functional Programming #18:
-#   Mixing Functional and Procedural => Job Security
-function accessible_modules(mod, ret = Set())
-  push!(ret, mod)
-  map(m->accessible_modules(m, ret), setdiff(module_usings(mod), ret))
-  return ret
-end
-
 accessible_names(mod = Main) =
-  @as _ mod accessible_modules(_, Set()) map(names, _) [_...] Set
+  @as _ mod [_, module_usings(_)...] map(names, _) [_...]
 
 handle("editor.julia.hints") do req, data
-  editor_command(req, "hints", {:hints => map(string, accessible_names())})
+  editor_command(req, "hints", {:hints => [string(n) for n in accessible_names()]})
 end
