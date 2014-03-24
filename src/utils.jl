d9f833f8ea6dfc550bbf1ba28abc022d6fde8d3a
Mike Innes <mike.j.innes@gmail.com>
2014-03-24 00:33:38 +0000

    add with_out_str

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,24 @@
-Base.in(name, m::Module) = name in names(m)
+#jewel module Jewel
+
+Base.in(name::Symbol, m::Module) = isdefined(m, name)
 
 Base.get(m::Module, name) = m.(name)
 
-Base.get(m::Module, name, default) =
+Base.get(m::Module, name::Symbol, default) =
   name in m ? get(m, name) : default
 
+Base.get(m::Module, name, default) = default
+
+function with_out_str(f::Function)
+  orig_stdout = STDOUT
+  rd, wr = redirect_stdout()
+  f()
+  redirect_stdout(orig_stdout)
+  return readavailable(rd)
+end
+
+macro with_out_str(expr)
+  :(with_out_str(()->$expr)) |> esc
+end
+
+help_str(x) = @with_out_str help(x)
