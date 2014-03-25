2d11eeccee3953f120f6291a6075c83d72520efb
Mike Innes <mike.j.innes@gmail.com>
2014-03-25 01:48:12 +0000

    support Module.name completion

--- src/completions.jl
+++ src/completions.jl
@@ -1,6 +1,6 @@
 #jewel module Jewel
 
-const builtins = ["begin", "function", "type", "immutable", "let", "macro", "for", "while", "quote", "if", "else", "elseif", "try", "finally", "catch", "do", "end", "else", "elseif", "catch", "finally"]
+const builtins = ["begin", "function", "type", "immutable", "let", "macro", "for", "while", "quote", "if", "else", "elseif", "try", "finally", "catch", "do", "end", "else", "elseif", "catch", "finally", "true", "false"]
 
 module_usings(mod) = ccall(:jl_module_usings, Any, (Any,), mod)
 
@@ -12,17 +12,30 @@ accessible_names(mod = Main) =
 packages() =
   @>> Pkg.dir() readdir filter(x->!ismatch(r"^\.|^METADATA$|^REQUIRE$", x))
 
-# TODO: Module.name
+function get_submodule(mod, names)
+  sub = nothing
+  for m in names
+    sub = get(mod, m, nothing)
+    sub == nothing && break
+  end
+  return sub
+end
 
 handle("editor.julia.hints") do req, data
   cur_line = lines(data["code"])[data["cursor"]["line"]]
+  qualified = @> cur_line get_qualified_name(data["cursor"]["col"]) split(".") (x->map(symbol, x))
 
-  if ismatch(r"^using ", cur_line) # Staight after using
-    editor_command(req, "hints", {:hints => packages()})
+  ismatch(r"^using ", cur_line) && # Staight after using
+    return editor_command(req, "hints", {:hints => packages()})
 
-  else
-    mod = get_module_name(lines(data["code"]), data["cursor"]["line"])
-    mod = get(Main, mod, Main)
-    editor_command(req, "hints", {:hints => [string(n) for n in accessible_names(mod)]})
+  mod = get_module_name(lines(data["code"]), data["cursor"]["line"])
+  mod = get(Main, mod, Main)
+
+  if length(qualified) > 1
+    sub = get_submodule(mod, qualified[1:end-1])
+    sub != nothing &&
+      return editor_command(req, "hints", {:hints => [string(n) for n in names(sub, true)], :notextual => true})
   end
+
+  return editor_command(req, "hints", {:hints => [string(n) for n in accessible_names(mod)]})
 end
