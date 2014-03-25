40ee2155bc656315f4ea341cd6815e5562ad1ec5
Mike Innes <mike.j.innes@gmail.com>
2014-03-25 07:37:24 +0000

    make autocomplete extensible

--- src/completions.jl
+++ src/completions.jl
@@ -3,7 +3,7 @@
 const builtins = ["begin", "function", "type", "immutable", "let", "macro",
                   "for", "while", "quote", "if", "else", "elseif", "try",
                   "finally", "catch", "do", "end", "else", "elseif", "catch",
-                  "finally", "true", "false"]
+                  "finally", "true", "false", "using"]
 
 module_usings(mod) = ccall(:jl_module_usings, Any, (Any,), mod)
 
@@ -11,11 +11,6 @@ accessible_names(mod = Main) =
   [names(mod, true, true),
    map(names, module_usings(mod))...,
    builtins] |> unique
-
-packages(dir = Pkg.dir()) =
-  @>> dir readdir filter(x->!ismatch(r"^\.|^METADATA$|^REQUIRE$", x))
-
-packages_all() = packages(Pkg.dir("METADATA"))
 
 function get_submodule(mod, names)
   sub = nothing
@@ -26,24 +21,58 @@ function get_submodule(mod, names)
   return sub
 end
 
+const completions = Dict{String,Function}()
+
+complete(f, s) = completions[s] = f
+
 handle("editor.julia.hints") do req, data
   cur_line = lines(data["code"])[data["cursor"]["line"]]
-  qualified = @> cur_line get_qualified_name(data["cursor"]["col"]) split(".") (x->map(symbol, x))
-
-  beginswith(cur_line, "using ") &&
-    return editor_command(req, "hints", {:hints => packages(), :notextual => true})
-
-  beginswith(cur_line, "Pkg.add(") &&
-    return editor_command(req, "hints", {:hints => packages(), :notextual => true})
+  cur_line |> isempty && return
+  pos = data["cursor"]["col"]
 
   mod = get_module_name(lines(data["code"]), data["cursor"]["line"])
   mod = get(Main, mod, Main)
 
+  qualified = @> cur_line get_qualified_name(pos) split(".") (x->map(symbol, x))
+
+  # Module.name completions
   if length(qualified) > 1
     sub = get_submodule(mod, qualified[1:end-1])
     sub != nothing &&
       return editor_command(req, "hints", {:hints => [string(n) for n in names(sub, true)], :notextual => true})
   end
 
+  # Specific completions
+  for (s, f) in completions
+    ret = f(cur_line, pos)
+    ret in (nothing, false) || return editor_command(req, "hints", ret)
+  end
+
+  # Otherwise, suggest all accessible names
   return editor_command(req, "hints", {:hints => [string(n) for n in accessible_names(mod)]})
+end
+
+# Package Completions
+
+packages(dir = Pkg.dir()) =
+  @>> dir readdir filter(x->!ismatch(r"^\.|^METADATA$|^REQUIRE$", x))
+
+complete("using") do line, pos
+  beginswith(line, "using ") &&
+    {:hints => packages(), :notextual => true}
+end
+
+all_packages() = packages(Pkg.dir("METADATA"))
+
+complete("pkg-add") do line, pos
+  beginswith(line, "Pkg.add(") &&
+    {:hints => all_packages(), :notextual => true}
+end
+
+req_packages() =
+  @>> Pkg.dir("REQUIRE") readall lines
+
+complete("pkg-rm") do line, pos
+  beginswith(line, "Pkg.rm(") &&
+    {:hints => req_packages(), :notextual => true}
 end
