841390a743664bc5526df76be3390bdab24c32dc
Mike Innes <mike.j.innes@gmail.com>
2014-06-22 11:43:26 +0100

    Add module-finding functionality

--- src/Jewel.jl
+++ src/Jewel.jl
@@ -4,6 +4,7 @@ using Lazy
 
 include("io.jl")
 include("eval.jl")
+include("module.jl")
 
 # Shim for now
 server(args...) = Main.LightTable.server(args...)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,5 @@ using Base.Test, Jewel
 
 @test get_thing("Base.fft") == fft
 @test get_thing(Base, [:fft]) == fft
+
+@test file_module(Pkg.dir("Jewel", "src", "module.jl")) == "Jewel"
