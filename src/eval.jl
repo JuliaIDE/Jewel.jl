add32a8017ae270705b35d87baedd7fad9d1434f
Mike Innes <mike.j.innes@gmail.com>
2014-06-24 20:03:20 +0100

    add get_module

--- src/eval.jl
+++ src/eval.jl
@@ -35,5 +35,10 @@ end
 
 # Get the current module for a file/pos
 
-# function get_module(code::String, )
-# end
+function get_module(file::String, code::String, pos)
+  filem = file_module(file)
+  codem = code_module(code, pos)
+  modstr = (codem != "" && filem != "") ? "$codem.$filem" :
+           codem == "" ? filem : codem
+  get_thing(modstr, Main)
+end
