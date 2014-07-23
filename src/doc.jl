5e792d909e7fa83f203a4f93dccc0e2f68166a06
Michael Hatherly <michaelhatherly@gmail.com>
2014-07-23 22:24:11 +0200

    LaTeX command lookup for unicode characters.

--- src/doc.jl
+++ src/doc.jl
@@ -7,11 +7,22 @@ function thingorfunc(code, cursor, mod = Main)
 end
 
 function doc(code, cursor, mod = Main)
+  docs = String[]
+
   thing = thingorfunc(code, cursor, mod)
-  thing == nothing && return
-  help = helpstr(thing)
-  help != "No help information found.\n" && return help
-  return
+  thing == nothing || push!(docs, helpstr(thing))
+
+  texcmd = texcommand(code, cursor)
+  texcmd == nothing || push!(docs, texcmd)
+
+  help = join(docs, "\n\n")
+  help in ("No help information found.\n", "") ? nothing : help
+end
+
+function texcommand(code, cursor)
+  sym = string(charundercursor(code, cursor))
+  cmd = get(reverse_latex_commands, sym, "")
+  isempty(cmd) ? nothing : "LaTeX command for \"$(sym)\" is \"$(cmd)\"."
 end
 
 function methodsorwith(code, cursor, mod = Main)
