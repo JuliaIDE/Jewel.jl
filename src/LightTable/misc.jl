ca966eaf336fe04599eff541de1fce17a4b7f09d
Mike Innes <mike.j.innes@gmail.com>
2014-08-08 03:36:05 +0100

    utilities to add Julia to the path

--- src/LightTable/misc.jl
+++ src/LightTable/misc.jl
@@ -21,3 +21,19 @@ function displayinline!(req, tree::Jewel.ProfileView.ProfileTree, bounds)
                       :line => li.line,
                       :percent => p} for (li, p) in Jewel.ProfileView.fetch() |> Jewel.ProfileView.flatlines]})
 end
+
+# Add Julia to the path
+
+@unix_only begin
+  function addtopath!()
+    rmfrompath!()
+    path = joinpath(Base.JULIA_HOME, "julia")
+    @assert isfile(path)
+    run(`ln -s $path /usr/local/bin/julia`)
+  end
+
+  function rmfrompath!()
+    path = "/usr/local/bin/julia"
+    isfile(path) && rm(path)
+  end
+end
