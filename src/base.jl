d776ae1875d913690aeb9d35891c012f840dee19
Mike Innes <mike.j.innes@gmail.com>
2014-08-11 12:55:37 +0100

    add text type

--- src/base.jl
+++ src/base.jl
@@ -7,12 +7,18 @@ end
 
   # Display primitives
 
-  export HTML
+  export HTML, Text
 
   type HTML
     content::UTF8String
   end
 
   writemime(io::IO, ::MIME"text/html", h::HTML) = print(io, h.content)
+
+  type Text
+    content::UTF8String
+  end
+
+  writemime(io::IO, ::MIME"text/plain", t::Text) = print(io, t.content)
 
 end
