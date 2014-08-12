11f78873e49cdf5ff0cf3c8fab4c14823b192614
Mike Innes <mike.j.innes@gmail.com>
2014-08-12 14:56:03 +0100

    new eval-all code

--- src/LightTable/eval.jl
+++ src/LightTable/eval.jl
@@ -13,3 +13,19 @@ handle("editor.eval.julia") do editor, data
   mod = Jewel.getmodule(data["code"], bounds[1], filemod = data["module"])
   eval(editor, mod, code, file, bounds)
 end
+
+handle("editor.eval.julia.all") do editor, data
+  file = @or data["path"] "REPL"
+  code = data["code"]
+  mod = Jewel.getthing(data["module"], Main)
+  try
+    include_string(mod, code, file)
+  catch e
+    println(STDERR, "Error evaluating $file:")
+    showerror(STDERR, e.error, catch_backtrace())
+    println(STDERR)
+
+    notify_error(sprint(showerror, e.error))
+  end
+  notify_done()
+end
