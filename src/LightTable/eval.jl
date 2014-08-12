a9bd1732b7a76e51db0fd9496cd29ed7563b9443
Mike Innes <mike.j.innes@gmail.com>
2014-08-12 14:17:59 +0100

    factor out eval code

--- src/LightTable/eval.jl
+++ src/LightTable/eval.jl
@@ -1,14 +1,15 @@
-LNR.cursor(d::Dict) = cursor(d["line"], d["col"])
-
-handle("editor.eval.julia") do editor, data
-  file = @or data["path"] "REPL"
-  line = data["start"]["line"]
-  code, bounds = Jewel.getblock(data["code"], line)
-  mod = Jewel.getmodule(data["code"], line, filemod = data["module"])
+function eval(editor, mod, code, file, bounds)
   try
-    result = include_string(mod, code, file, line)
+    result = include_string(mod, code, file, bounds[1])
     displayinline!(editor, result, bounds)
   catch e
     showexception(editor, e.error, catch_backtrace(), bounds)
   end
+end
+
+handle("editor.eval.julia") do editor, data
+  file = @or data["path"] "REPL"
+  code, bounds = Jewel.getblock(data["code"], data["start"]["line"])
+  mod = Jewel.getmodule(data["code"], bounds[1], filemod = data["module"])
+  eval(editor, mod, code, file, bounds)
 end
