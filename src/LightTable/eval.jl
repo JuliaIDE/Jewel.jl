3d154809316d722d63208779c3f8e2214e3ee987
Mike Innes <mike.j.innes@gmail.com>
2014-08-12 13:44:46 +0100

    new eval code

--- src/LightTable/eval.jl
+++ src/LightTable/eval.jl
@@ -1,43 +1,14 @@
-function get_module(data::Dict)
-  mod = get_module_name(data)
-  mod == "" || return getthing(mod)
-  mod = data["module"]
-  mod == nothing || return getthing(mod)
-  return Main
-end
+LNR.cursor(d::Dict) = cursor(d["line"], d["col"])
 
-# ----
-# Eval
-# ----
-
-# Shoud be split into eval and eval.all
-# TODO: rewrite completely
-handle("editor.eval.julia") do req, data
-  info = get_code(data)
-  all = get(data, "all", false)
-
-  val = nothing
-  mod = info[:module] != nothing ? info[:module] :
-        data["module"] != nothing ? getthing(data["module"]) : Main
-
-  mod == nothing && error("Module $(data["mod"]) not found")
-
-  path = get(data, "path", nothing)
-  task_local_storage()[:SOURCE_PATH] = path
-  path == nothing && (path = "REPL")
-
+handle("editor.eval.julia") do editor, data
+  file = @or data["path"] "REPL"
+  line = data["start"]["line"]
+  code, bounds = Jewel.getblock(data["code"], line)
+  mod = Jewel.getmodule(data["code"], line, filemod = data["module"])
   try
-    val = include_string(mod, info[:code], path, info[:lines][1])
+    result = include_string(mod, code, file, line)
+    displayinline!(editor, result, bounds)
   catch e
-    showexception(req, sprint(io->showerror_html(io, e.error, catch_backtrace(), :include_string)), info[:lines])
-    return
-  end
-
-  if all
-    notify_done()
-    file = path == nothing ? "file" : splitdir(path)[2]
-    notify("✓ Evaluated $file in module $mod")
-  else
-    displayinline!(req, val, info[:lines])
+    showexception(editor, e.error, catch_backtrace(), bounds)
   end
 end
