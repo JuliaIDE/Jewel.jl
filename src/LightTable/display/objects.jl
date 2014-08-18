49b10cf4ec15cdd84aba4b691323a2adaa15f316
Mike Innes <mike.j.innes@gmail.com>
2014-08-18 20:59:54 +0100

    Start of collapsible results

--- src/LightTable/display/objects.jl
+++ src/LightTable/display/objects.jl
@@ -1,1 +1,23 @@
+import Base: writemime
+
 displayinline(::Nothing) = Text("✓")
+
+type Collapsible
+  header
+  content
+end
+
+function writemime(io::IO, m::MIME"text/html", c::Collapsible)
+  println(io, """<div class="collapsible">""")
+  println(io, """<span class="collapsible-header">""")
+  writemime(io, m, c.header)
+  println(io, """</span>""")
+  println(io, """<div class="collapsible-content">""")
+  writemime(io, m, c.content)
+  println(io, """</div></div>""")
+end
+
+displayinline!(req, html::Collapsible, bounds) =
+  showresult(req, stringmime("text/html", html), bounds, html=true)
+
+Collapsible(HTML("foo"), HTML("hello world"))
