60e95d3aebbc8ac4f3ca6a55d3d89b2c6e3b6674
Mike Innes <mike.j.innes@gmail.com>
2014-08-18 15:53:05 +0100

    move commands

--- src/LightTable/display/display.jl
+++ src/LightTable/display/display.jl
@@ -1,28 +1,4 @@
-# Editor commands
-
-function showresult(req, value::String, bounds::(Int, Int); under = false, html = false)
-  raise(req, "julia.result",
-        {"value" => value,
-         "start" => bounds[1],
-         "end"   => bounds[2],
-         "under" => under,
-         "html"  => html})
-end
-
-function showexception(req, value::String, bounds::(Int, Int))
-  raise(req, "julia.error",
-        {"value" => value,
-         "start" => bounds[1],
-         "end"   => bounds[2]})
-end
-
-function showexception(editor, e, bt, bounds::(Int, Int))
-  notify_error(sprint(showerror, e))
-  showexception(
-    editor,
-    sprint(showerror_html, e, bt, :include_string),
-    bounds)
-end
+include("commands.jl")
 
 # Display infrastructure
 
