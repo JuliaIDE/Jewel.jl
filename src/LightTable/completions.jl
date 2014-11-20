03a11641ccfcd56908aaa2006d2cd245d9e76ca3
Nils <nilshg@hotmail.com>
2014-11-20 15:34:45 +0000

    Compatibility to Julia v0.4

--- src/LightTable/completions.jl
+++ src/LightTable/completions.jl
@@ -1,14 +1,17 @@
+using Compat
 handle("editor.julia.hints") do editor, data
   cur = cursor(data["cursor"]["line"], data["cursor"]["col"])
   code = data["code"]
   mod = Jewel.getmodule(code, cur, filemod = data["module"])
   completions = allcompletions(code, cur, mod = mod, file = data["path"])
   if completions == nothing
+    temp = @compat Dict(:hints => [])
     raise(editor, "editor.julia.hints.update",
-          {:hints => {}})
+            temp)
   else
+    temp = @compat Dict(:hints => completions[:hints],
+           :pattern => completions[:pattern].pattern)
     raise(editor, "editor.julia.hints.update",
-          {:hints => completions[:hints],
-           :pattern => completions[:pattern].pattern})
+          temp)
   end
 end
