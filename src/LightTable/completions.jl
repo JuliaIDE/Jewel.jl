629569a2087d5e4368834a5e8d66855d74cb1b8c
Nils <nilshg@hotmail.com>
2014-11-22 14:55:01 +0000

    Changed to `@d()` syntax

--- src/LightTable/completions.jl
+++ src/LightTable/completions.jl
@@ -4,12 +4,12 @@ handle("editor.julia.hints") do editor, data
   mod = Jewel.getmodule(code, cur, filemod = data["module"])
   completions = allcompletions(code, cur, mod = mod, file = data["path"])
   if completions == nothing
-    temp = @compat Dict(:hints => [])
+    temp = @d(:hints => [])
     raise(editor, "editor.julia.hints.update",
             temp)
   else
-    temp = @compat Dict(:hints => completions[:hints],
-                        :pattern => completions[:pattern].pattern)
+    temp = @d(:hints => completions[:hints],
+              :pattern => completions[:pattern].pattern)
     raise(editor, "editor.julia.hints.update",
           temp)
   end
