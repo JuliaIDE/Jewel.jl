43939fd69e74f5dc72d3526008dc043eba3a0859
Nils <nilshg@hotmail.com>
2014-11-22 14:53:45 +0000

    Changed to `@d()` syntax

--- src/LightTable/eval.jl
+++ src/LightTable/eval.jl
@@ -21,9 +21,9 @@ function evaldisplay(editor, mod, code, file, bounds; data = Dict(), info = Dict
     result = eval(editor, mod, code, file, bounds)
     result === evalerr && return
     withcurrentresult(register_result(result, data)) do
-      displayinline!(result, @compat Dict(:editor => editor,
-                                          :bounds => bounds,
-                                          :info => merge(info, @compat Dict(:id => string(_currentresult_.id)))))
+      displayinline!(result, @d(:editor => editor,
+                                :bounds => bounds,
+                                :info => merge(info, @d(:id => string(_currentresult_.id)))))
     end
   catch e
     showexception(editor, e, catch_backtrace(), bounds)
@@ -49,12 +49,12 @@ handle("eval.block") do editor, data
   bounds = data["bounds"]
   mod = Jewel.getmodule(data["code"], bounds[1], filemod = data["module"])
   # We need some custom data to enable reevaluation
-  data = @compat Dict(:editor => editor,
-                           :mod => mod,
-                           :bounds => bounds,
-                           :code => code,
-                           :path => data["path"])
-  info = @compat Dict(:scales => bounds)
+  data = @d(:editor => editor,
+            :mod => mod,
+            :bounds => bounds,
+            :code => code,
+            :path => data["path"])
+  info = @d(:scales => bounds)
   evaldisplay(editor, mod, code, data["path"], bounds, data, info)
 end
 
@@ -76,9 +76,9 @@ end
 
 handle("editor.block") do editor, data
   block, bounds = Jewel.getblock(data["code"], data["line"])
-  temp = @compat Dict("block" => block,
-                      "bounds" => bounds,
-                      "id" => data["id"])
+  temp = @d("block" => block,
+            "bounds" => bounds,
+            "id" => data["id"])
   raise(editor, "return-block", temp)
 end
 
