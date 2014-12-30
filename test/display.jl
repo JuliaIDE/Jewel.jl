6b7c0e385cd7f676060bf94cd72522079f72faf7
Mike Innes <mike.j.innes@gmail.com>
2014-12-30 15:28:40 +0000

    fix for rounding 999s

--- test/display.jl
+++ test/display.jl
@@ -15,3 +15,7 @@ pi/10000
 @test round3(1/0.999) == "1.001"
 @test round3(1/9.95) == "0.101"
 @test round3(1/0.999) == "1.001"
+
+# The rounder currently bails out on these
+# round3(1.9999)
+# round3(0.09999)
