f444f179a6ea763ab3d696527a263e733753c8e8
Varanas <pfitzseb@physik.hu-berlin.de>
2014-12-31 14:44:47 +0100

    fix for wrong function call in tests

--- test/display.jl
+++ test/display.jl
@@ -17,6 +17,6 @@ pi/10000
 @test round3(1.9999) == "2.000"
 @test round3(0.09999) == "0.100" # should probably be 0.1000, but that's a minor problem
 @test round3(-1.1299e-6) == "-1.130e-6"
-@test round_disp(1.3579/100_000_000) == "1.358e-8"
+@test round3(1.3579/100_000_000) == "1.358e-8"
 @test round3(-Inf) == "-Inf"
 @test round3(NaN) == "NaN"
